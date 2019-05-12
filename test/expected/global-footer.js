6603c73fec266e622ea909119a68080c34b7e7d1
stanislav <stankim.business@gmail.com>
2019-05-11 20:28:36 -0700

    Design	Verify that design looks like on example picture in Bug Tracker requirements(still pending) 	Verify that text align: center (done); 	Verify that background color: rgba(0,0,0,0.125) (done); 	Verify that font size: 16px/1 rem (done); 	Verify that font family: Segoe UI (done); 	Verify that font weight: 400 (done); 	Verify that font color: #212529 (done);

---
+++ test/expected/global-footer.js
@@ -0,0 +1,10 @@
+export default {
+    text: '© 2019 Bug Tracker by React Web Development', 
+    version: 'Version: 0.3.40',
+    textAlign: 'center',
+    colorValue: 'rgba(0,0,0,0.125)',
+    fSize:'16px',
+    fFamily:'segoe ui',
+    fWeight: 400,
+    fColor: '#212529'
+}
---
+++ test/selectors/global-footer.js
@@ -0,0 +1,5 @@
+export default {
+    footer: '.custom-footer',
+    text: '//*[@id="root"]/div/div[3]/div/div',
+    smalltext: '.small-text',
+}
