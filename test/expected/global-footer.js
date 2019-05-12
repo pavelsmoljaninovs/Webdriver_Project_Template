724dab154d1da399f27b21606fc0afe91f23f94d
stanislav <stankim.business@gmail.com>
2019-05-11 21:23:41 -0700

    made files similar to @amadjouga

--- test/expected/global-footer.js
+++ test/expected/global-footer.js
@@ -1,6 +1,10 @@
 export default {
     text: '© 2019 Bug Tracker by React Web Development', 
     version: 'Version: 0.3.40',
+    currentVersion: ‘0.3.40’,
+    releaseVersion: ‘0’,
+    sprint: ‘3’,
+    year: ‘2019’
     textAlign: 'center',
     colorValue: 'rgba(0,0,0,0.125)',
     fSize:'16px',
