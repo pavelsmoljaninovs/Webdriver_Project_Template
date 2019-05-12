fef19cbf1c4ac8f5b261e3f420a96657dd24a92a
mainuser <ytifkb81>
2019-05-11 21:22:46 -0700

    made files similar with @StanislavKim1

--- test/expected/global-footer.js
+++ test/expected/global-footer.js
@@ -6,5 +6,9 @@ export default {
     sprint: '3',
     year: '2019',
     textAlign: 'center',
-
+    colorValue: 'rgba(0,0,0,0.125)',
+    fSize:'16px',
+    fFamily:'segoe ui',
+    fWeight: 400,
+    fColor: '#212529'
 }
