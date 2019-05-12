18c9c3f5af30807a184f336996039cfe1dea299e
mainuser <ytifkb81>
2019-05-11 23:56:41 -0700

    Made all requested changes

--- test/specs/reg/global-footer/general.js
+++ test/specs/reg/global-footer/general.js
@@ -1,18 +1,19 @@
 import {assert} from 'chai';
 import sel from '../../../selectors/global-footer';
 import exp from '../../../expected/global-footer';
+import disp from '../../../helpers/helpers';
 
 
 describe('General', function () {
 
     it('Footer id displayed', function () {
         browser.url('/');
-        let footer = $(sel.footer).isDisplayed();
+        let footer = disp.isVisible(sel.footer);
         assert.isTrue(footer);
     });
     it('Two lines text', function () {
-        let line_1 = $(sel.text).isDisplayed();
-        let line_2 = $(sel.smalltext).isDisplayed();
+        let line_1 = disp.isVisible(sel.text);
+        let line_2 = disp.isVisible(sel.smallText);
         assert.isTrue(line_1);
         assert.isTrue(line_2);
     });
@@ -21,23 +22,23 @@ describe('General', function () {
         assert.equal(text, exp.text);
     });
     it('Version included', function () {
-        let smalltext = $(sel.smalltext).getText();
+        let smalltext = $(sel.smallText).getText();
         assert.equal(smalltext, exp.version);
     });
     it('Current version', function () {
-        let currentVersion = $(sel.smalltext).getText().slice(9,16);
+        let currentVersion = $(sel.smallText).getText().slice(9,16);
         assert.equal(currentVersion, exp.currentVersion);
     });
     it('Release version', function () {
-        let releaseVersion = $(sel.smalltext).getText().slice(9,10);
+        let releaseVersion = $(sel.smallText).getText().slice(9,10);
         assert.equal(releaseVersion, exp.releaseVersion);
     });
     it('Sprint number', function () {
-        let sprint = $(sel.smalltext).getText().slice(11,12);
+        let sprint = $(sel.smallText).getText().slice(11,12);
         assert.equal(sprint, exp.sprint);
     });
     it('Sprint number', function () {
-        let sprint = $(sel.smalltext).getText().slice(11,12);
+        let sprint = $(sel.smallText).getText().slice(11,12);
         assert.equal(sprint, exp.sprint);
     });
 
