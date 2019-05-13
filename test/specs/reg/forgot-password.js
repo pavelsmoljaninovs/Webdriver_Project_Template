17316ebc3569207c0e3a82713c0f25178f2b41e7
ytuzov <pachkovska@gmail.com>
2019-05-12 21:50:41 -0700

    added selectors as variables

--- test/specs/reg/forgot-password.js
+++ test/specs/reg/forgot-password.js
@@ -1,5 +1,6 @@
 import { assert } from 'chai';
 import exp from '../../expected/forgot-password';
+import sel from '../../selectors/forgot-password';
 
 //Forgot password back button tests
 
@@ -7,46 +8,35 @@ describe('Forgot Password Back Button', function () {
 
     it('Back Button Text Font Size', function () {
         browser.url('/');
-        $('#forgot').click();
-        let backButton = $('#back');
+        $(sel.forgot).click();
+        let backButton = $(sel.backButton);
         let fontSize = backButton.getCSSProperty('font-size').parsed.value;
         assert.equal(fontSize, exp.fontSize);
     });
 
     it('Back Button Text Font Weight', function () {
-        browser.url('/');
-        $('#forgot').click();
-        let backButton = $('#back');
+        let backButton = $(sel.backButton);
         let fontWeight = backButton.getCSSProperty('font-weight').parsed.value;
         assert.equal(fontWeight, exp.fontWeight);
     });
 
     it('Back Button Text Font Family', function () {
-        browser.url('/');
-        $('#forgot').click();
-        let backButton = $('#back');
+        let backButton = $(sel.backButton);
         let fontFamily = backButton.getCSSProperty('font-family').value;
         assert.equal(fontFamily, exp.fontFamily);
     });
 
     it('Back Button Text Font Color', function () {
-        browser.url('/');
-        $('#forgot').click();
-        let backButton = $('#back');
+        let backButton = $(sel.backButton);
         let fontColor = backButton.getCSSProperty('color').parsed.hex;
-        console.log(fontColor);
         assert.equal(fontColor, exp.fontColor);
     });
 
     it('Back Button Color', function () {
-        browser.url('/');
-        $('#forgot').click();
-        let backButton = $('#back');
+        let backButton = $(sel.backButton);
         let buttonColor = backButton.getCSSProperty('background-color').parsed.hex;
-        console.log(buttonColor);
         assert.equal(buttonColor, exp.buttonColor);
     });
-
 });
 
 
