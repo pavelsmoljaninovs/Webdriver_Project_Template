c38d67e8c9ae80f5be993a92712edf33d9f5a044
ira <itrusevich@gmail.com>
2019-05-12 21:07:39 -0700

    created login selector, removed browser refresh from scripts

--- test/specs/reg/login/error.js
+++ test/specs/reg/login/error.js
@@ -7,55 +7,43 @@ describe('Design', function () {
 
     it('Background Color', function () {
         browser.url('/');
-        $('#login').click();
+        $(sel.login).click();
         let error = $(sel.error);
         let bgColor = error.getCSSProperty('background-color').parsed.hex;
         assert.equal(bgColor, exp.errorBackgroundColor);
     });
 
     it('Border Color', function () {
-        browser.url('/');
-        $('#login').click();
         let error = $(sel.error);
         let borderColor = error.getCSSProperty('border-color').parsed.hex;
         assert.equal(borderColor, exp.errorBorderColor);
     });
 
     it('Font Family', function () {
-        browser.url('/');
-        $('#login').click();
         let error = $(sel.error);
         let fontFamily = error.getCSSProperty('font-family').parsed.value;
         assert.equal(fontFamily[0], exp.errorFontFamily.toLowerCase());
     });
 
     it('Font Size', function () {
-        browser.url('/');
-        $('#login').click();
         let error = $(sel.error);
         let fontSize = error.getCSSProperty('font-size').value;
         assert.equal(fontSize, exp.errorFontSize);
     });
 
     it('Font Weight', function () {
-        browser.url('/');
-        $('#login').click();
         let error = $(sel.error);
         let fontWeight = error.getCSSProperty('font-weight').value;
         assert.equal(fontWeight, exp.errorFontWeight);
     });
 
     it('Font Color', function () {
-        browser.url('/');
-        $('#login').click();
         let error = $(sel.error);
         let fontColor = error.getCSSProperty('color').parsed.hex;
         assert.equal(fontColor, exp.errorFontColor);
     });
 
     it('Text Align', function () {
-        browser.url('/');
-        $('#login').click();
         let error = $(sel.error);
         let textAlign = error.getCSSProperty('text-align').value;
         assert.equal(textAlign, exp.errorTextAlign);
