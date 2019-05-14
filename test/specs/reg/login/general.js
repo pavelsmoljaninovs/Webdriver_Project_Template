d0873afdc12677d7f79a74ee64ce4d19fd8b5e71
Volodymyr-Cherniak <vovastalker26@gmail.com>
2019-05-14 20:24:58 +0300

    add all new tests

---
+++ test/specs/reg/login/general.js
@@ -0,0 +1,47 @@
+import {assert} from "chai";
+import sel from "../../../selectors/login";
+import exp from "../../../expected/login";
+
+describe('Forgot Password link', function () {
+
+    it('Link name', function () {
+        browser.url('/');
+        let forgotPas = $(sel.forgotPassword);
+        let linkName = forgotPas.getText('.button_submit');
+        console.log(linkName);
+        assert.equal(linkName, exp.forgotPasswordLinkLinkName);
+    });
+
+    it('"Forgot Password link" is displayed ', function () {
+        let forgotPas = $(sel.forgotPassword);
+        let actual = forgotPas.isDisplayed();
+        assert.equal(actual, true);
+    });
+
+    it('Link type', function () {
+        let forgotPas = $(sel.forgotPassword);
+        let linkType = forgotPas.getAttribute('class');
+        assert.equal(linkType, exp.forgotPasswordLinkLinkType);
+    });
+
+    it('Font Color', function () {
+        let forgotPas = $(sel.forgotPassword);
+        let fontColor = forgotPas.getCSSProperty('color').parsed.hex;
+        assert.equal(fontColor, exp.forgotPasswordLinkColor);
+    });
+
+    it('Text-decoration is underline', function () {
+        let forgotPas = $(sel.forgotPassword);
+        forgotPas.moveTo();
+        let textDecor = forgotPas.getCSSProperty('text-decoration-line').value;
+        assert.equal(textDecor, exp.forgotPasswordLinkTextDecoration);
+    });
+
+    it('After clik, open page "Forgot Password" ', function () {
+        let forgotPas = $(sel.forgotPassword);
+        forgotPas.click();
+        let actual = $('#remind').isDisplayed();
+        assert.equal(actual, true);
+    });
+
+});
