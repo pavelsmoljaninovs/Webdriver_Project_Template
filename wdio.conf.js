e8b940871cb27c66c5ac110d548a652e19ad23fb
Volodymyr-Cherniak <vovastalker26@gmail.com>
2019-05-14 18:36:22 +0300

    aвdd small change

--- wdio.conf.js
+++ wdio.conf.js
@@ -14,7 +14,7 @@ exports.config = {
         './test/specs/reg/registration/functionality.js',
         './test/specs/reg/login/error.js',
         './test/specs/reg/forgot-password.js',
-        './test/specs/reg/navigationBar/general-nav-bar.js'
+        './test/specs/reg/navigationBar/general-nav-bar.js',
         './test/specs/reg/login/general.js'
     ],
 
