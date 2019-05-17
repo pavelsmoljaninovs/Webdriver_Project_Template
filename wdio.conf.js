773c69981b4b5028eadd86463eed0db165fa7344
marinabasistaya <marinabasistaya@gmail.com>
2019-05-17 18:44:09 -0500

    bug-form summary tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,12 +5,11 @@ exports.config = {
   specs: [
     //Add your spec between these two lines. Do not forget comma.
     //======================================
-   './test/specs/reg/bug-form/summary.js'
+     './test/specs/reg/bug-form/summary.js',
     //======================================
     './test/specs/reg/*.js',   //DO NOT REMOVE. This is short path to run all the specs.
     './test/specs/reg/**/*.js' //When you're writing new tests, just add you line between the lines above.
   ],
-
   exclude: [
     // 'path/to/excluded/files'
   ],
