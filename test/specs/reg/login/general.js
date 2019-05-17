a174590d4ea6ac9f2c046e910df63b56ebcfbbf2
Anna Gerasimova <gerasimova.hanna@gmail.com>
2019-05-16 22:12:44 -0700

    Merge changes v2

--- test/specs/reg/login/general.js
+++ test/specs/reg/login/general.js
@@ -14,57 +14,51 @@ describe('Login', function () {
     })
 
     it('Font size', function () {
-      browser.url('/');
       let input = $('#email');
       let fontSize = input.getCSSProperty('font-size').value;
       assert.equal(fontSize, exp.emailFontSize);
     })
 
     it('Font weight', function () {
-      browser.url('/');
       let input = $('#email');
       let fontWeight = input.getCSSProperty('font-weight').parsed.value;
       assert.equal(fontWeight, exp.emailFontWeight);
     })
 
     it('Placeholder font color', function () {
-      browser.url('/');
       let input = $('#placeholder');
       let fontWeight = input.getCSSProperty('color').parsed.hex;
       assert.equal(fontWeight, exp.emailPlaceholderColor);
     })
 
     it('Font family', function () {
-      browser.url('/');
       let input = $('#email');
       let fontFamily = input.getCSSProperty('font-family').value.toLowerCase();
       assert.equal(fontFamily, exp.emailFontFamily);
     })
 
-    it('Max chars', function () {
-      browser.url('/');
-      let input = $('#email');
-      input.addValue('j'.repeat(exp.emailMaxLength + 1));
-      let dataLength = input.getValue().length;
-      assert.equal(dataLength, exp.emailMaxLength);
-    })
-
     it('Focus highlight color', function () {
-      browser.url('/');
+        browser.url('/');
       let input = $('#email');
       input.click();
       let highlightColor = input.getCSSProperty('box-shadow').parsed.hex;
       assert.equal(highlightColor, exp.emailHighlightColor);
     })
 
-    it('Enters first symbol: email font color, function () {
-      browser.url('/');
+    it('Enters first symbol: email font color', function () {
       let input = $('#email');
       input.addValue('a');
       let fontColor = input.getCSSProperty('color').parsed.hex;
       assert.equal(fontColor, exp.emailValueTextColor);
     })
 
+    it('Max chars', function () {
+      let input = $('#email');
+      input.addValue('j'.repeat(exp.emailMaxLength + 1));
+      let dataLength = input.getValue().length;
+      assert.equal(dataLength, exp.emailMaxLength);
+    })
+    
   });
   
   describe('"* Required field" text', function () {
