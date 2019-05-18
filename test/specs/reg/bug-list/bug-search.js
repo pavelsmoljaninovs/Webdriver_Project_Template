0eabc923ed9373836edb6a450344520db7a61b98
vianat <vianatdev@gmail.com>
2019-05-18 12:04:21 -0500

    tests for search functionality

--- test/specs/reg/bug-list/bug-search.js
+++ test/specs/reg/bug-list/bug-search.js
@@ -2,6 +2,7 @@ import {assert} from 'chai';
 import help from '../../../helpers/helpers';
 import sel from '../../../selectors/bug-list';
 import exp from '../../../expected/bug-list';
+import loginData from "../../../data/login";
 
 describe('bug search', function () {
 
@@ -40,4 +41,58 @@ describe('bug search', function () {
 
     });
 
+    describe('search functionality', function () {
+
+        it('correct search result', function () {
+            help.login();
+            let fontsize = $$(sel.allColumnRows);
+            let allRowsNamesInString = "";
+            for(let el of fontsize){
+                allRowsNamesInString += el.getAttribute('title');
+                allRowsNamesInString += " ";
+            }
+
+            let splitArr = allRowsNamesInString.split(" ");
+            let arrLength = splitArr.length-2;
+
+            function randomInteger(min, max) {
+                let rand = min + Math.random() * (max - min);
+                rand = Math.round(rand);
+                return rand;
+            }
+
+            let checkStr = splitArr[randomInteger(0,arrLength)];
+            let foo = $(sel.searchField);
+            foo.setValue(checkStr);
+
+            let searchResult = $$(sel.allColumnRows);
+
+            let flag = true;
+            for(let el of searchResult){
+                if( !el.getAttribute('title').includes(checkStr) ) flag = false;
+            }
+            if(flag) assert.equal(true, true);
+
+        });
+
+        it('correct search result when search field is empty', function () {
+            help.login(); // dont remove
+            let allSearchResultLength = $$(sel.allColumnRows).length;
+
+            let foo = $(sel.searchField);
+            foo.setValue('test');
+
+            let SearchResultLength = $$(sel.allColumnRows).length;
+
+            if(allSearchResultLength !== SearchResultLength){
+                help.login(); // dont remove
+                let newResult = $$(sel.allColumnRows).length;
+                assert.equal(allSearchResultLength, newResult);
+            }
+
+        });
+
+    });
+
 });
+git
