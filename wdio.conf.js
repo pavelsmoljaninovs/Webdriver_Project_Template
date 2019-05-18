1f870ea25ec9fca0fb23b9a37b3ccfc35d53e0d1
OlgaFi <48150395+OlgaFi@users.noreply.github.com>
2019-05-18 17:07:50 +0200

    date&time

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,6 +5,7 @@ exports.config = {
   specs: [
     './test/specs/reg/*.js', //All suites instead of putting all the files here.
     './test/specs/reg/**/*.js' //Just comment those two lines and add your file for testing.
+    './test/specs/reg/bug-report/comment.js'
     //just remove your file before PR and two lines above will cover all the files in reg folder.
   ],
 
