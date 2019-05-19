e7db590724ed78ae1469cff5b4fb44b084234492
Viktoryia A <alekseevavikt@gmail.com>
2019-05-18 18:18:42 -0700

    add some  tests for login button

--- test/specs/reg/login/general.js
+++ test/specs/reg/login/general.js
@@ -3,7 +3,7 @@ import sel from '../../../selectors/login';
 import exp from '../../../expected/login';
 import help from "../../../helpers/helpers";
 
-describe('Email', function () {
+/*describe('Email', function () {
 
   it('Email field has a placeholder text when field is empty', function () {
     browser.url('/');
@@ -131,6 +131,52 @@ describe('Forgot Password link', function () {
     let actual = $('#remind').isDisplayed();
     assert.equal(actual, true);
   });
+
+}); */
+
+describe('Login Button', function () {
+
+   it('Login Button name', function () {browser.url('/');
+    let element = $(sel.login).getText();
+    assert.equal(element, exp.loginButtonText);
+   });
+
+   it('Font size',function () {
+        let fontSize = $(sel.login).getCSSProperty('font-size').parsed.value;
+        assert.equal(fontSize, exp.loginButtonFontSize);
+   });
+
+   it('Font size',function () {
+    let fontSize = $(sel.login).getCSSProperty('font-size').parsed.value;
+    assert.equal(fontSize, exp.loginButtonFontSize);
+   });
+
+   it('Font weight',function () {
+    let fontWeight = $(sel.login).getCSSProperty('font-weight').parsed.value;
+    assert.equal(fontWeight, exp.loginButtonFontWeight);
+   });
+
+   it('Font family',function () {
+     let fontFamily = $(sel.login).getCSSProperty('font-family').value;
+     assert.equal(fontFamily, exp.loginButtonFontFamily);
+    });
+
+    it('Font Color',function () {
+      let fontColor = $(sel.login).getCSSProperty('color').parsed.hex;
+      assert.equal(fontColor, exp.loginButtonFontColor);
+    });
+
+    it('Button Color',function () {
+        let buttonColor = $(sel.login).getCSSProperty('background-color').parsed.hex;
+        assert.equal(buttonColor, exp.loginButtonButtonColor);
+    });
+
+    it('Hover Color',function () {
+        $(sel.login).moveTo();
+        browser.pause(200);
+        let hoverColor = $(sel.login).getCSSProperty('background-color').parsed.hex;
+        assert.equal(hoverColor, exp.loginButtonHoverColor);
+    });
 
 });
 
