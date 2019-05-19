8355ebac669f6345ade19588d3e9fabc2de62df0
Anna Gerasimova <gerasimova.hanna@gmail.com>
2019-05-18 23:09:02 -0700

    minor changes

--- test/specs/reg/bug-form/text-area-steps-to-reproduce.js
+++ test/specs/reg/bug-form/text-area-steps-to-reproduce.js
@@ -6,17 +6,4 @@ import data from "../../../data/registration";
 
 describe('Steps to reproduce', function () {
 
-    it('Text area accept any text', function () {
-        assert.isTrue(help.randomText(sel.text));
-    });
-
-    it('Text area accept any text', function () {
-        help.openRegistration();
-        help.registration();
-        help.fieldClear(sel.firstName);
-        $(sel.firstName).addValue(data.maxNameLength);
-        $(sel.registerMe).click();
-        let newPage = $(sel.newBugButton);
-        newPage.waitForDisplayed(5000);
-        assert.isTrue(help.isVisible(sel.newBugButton));
-    });
+});
