ca91e3bd901b89753dd25374838975542a7d4c30
Anna Gerasimova <gerasimova.hanna@gmail.com>
2019-05-18 23:12:19 -0700

    minor changes2

--- test/specs/reg/bug-form/text-area-steps-to-reproduce.js
+++
@@ -1,9 +0,0 @@
-import { assert } from 'chai';
-import help from '../../../helpers/helpers';
-import sel from '../../../selectors/registration';
-import exp from '../../../expected/registration';
-import data from "../../../data/registration";
-
-describe('Steps to reproduce', function () {
-
-});
