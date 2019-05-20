c9360bf1b5087ce0192f93cc095254f43806fe1b
Iryna <iruna.manchak@gmail.com>
2019-05-19 23:11:08 -0700

    revert unnecessary changes

--- test/specs/reg/bug-form/bug-form-buttons.js
+++ test/specs/reg/bug-form/bug-form-buttons.js
@@ -1,133 +1,67 @@
-import {assert, expect} from 'chai';
-import sel from '../../selectors/bug-form-functionality';
-import exp from '../../expected/bug-form-functionality';
-import help from '../../helpers/helpers';
-import bform from '../../helpers/bug-form';
+import { assert } from 'chai';
+import sel from '../../../selectors/bug-form';
+import exp from '../../../expected/bug-form';
+import help from "../../../helpers/helpers";
 
-const waitForDisplayedTimeout = 5000;
+describe('General', function () {
 
-describe('Functionality of drop-down lists ', function () {
+  it('Font Family', function () {
+    help.login();
+    $(sel.newBugBut).click();
+    let fontFamily = $(sel.submitBut).getCSSProperty('font-family').value;
+    assert.equal(fontFamily, exp.fontFamily);
+  });
 
-    it('Verify that Severity drop-down placeholder is correct', function () {
-        help.login();
-        $(sel.newBug).click();
-        let severity = $(sel.severity).getText();
-        assert.equal(severity, exp.severity);
-    });
+  it('Font Size', function () {
+    let fontSize = $(sel.submitBut).getCSSProperty('font-size').parsed.value;
+    assert.equal(fontSize, exp.fontSize);
+  });
 
-    it('Verify contains of Severity drop-down list', function () {
-        let severity = $(sel.severity);
-        severity.click();
-        let severityAtr = $(sel.severityButtonOpen);
-        let divs = severityAtr.$$('div');
-        const arrayDiv = divs.map(div => div.getText());
-        expect(arrayDiv).to.eql(exp.severityList);
-    });
+  it('Font Weight', function () {
+    let fontWeight = $(sel.submitBut).getCSSProperty('font-weight').parsed.value;
+    assert.equal(fontWeight, exp.fontWeight);
+  });
 
-    it('Verify that Assignee drop-down placeholder is correct', function () {
-        let assignee = $(sel.assignee).getText();
-        assert.equal(assignee, exp.assignee);
-    });
+  it('Font Color', function () {
+    let fontColor = $(sel.submitBut).getCSSProperty('color').parsed.hex;
+    assert.equal(fontColor, exp.fontColor);
+  });
 
-    it('Verify that Priority drop-down placeholder is correct', function () {
-        let priority = $(sel.priority).getText();
-        assert.equal(priority, exp.priority);
-    });
-
-    it('Verify contains of Priority drop-down list', function () {
-        let priority = $(sel.priority);
-        priority.click();
-        let priorityAtr = $(sel.priorityButtonOpen);
-        let divs = priorityAtr.$$('div');
-        const arrayDiv = divs.map(div => div.getText());
-        expect(arrayDiv).to.eql(exp.priorityList);
-    });
-
-    it('Verify that Repro drop-down placeholder is correct', function () {
-        let repro = $(sel.repro).getText();
-        assert.equal(repro, exp.repro);
-    });
-
-    it('Verify contains of Repro drop-down list', function () {
-        let priority = $(sel.priority);
-        priority.click();
-        let repro = $(sel.repro);
-        repro.click();
-        let reproAtr = $(sel.reproButtonOpen);
-        let divs = reproAtr.$$('div');
-        const arrayDiv = divs.map(div => div.getText());
-        expect(arrayDiv).to.eql(exp.reproList);
-    });
+  it('Text Align', function () {
+    let textAlign = $(sel.submitBut).getCSSProperty('text-align').value;
+    assert.equal(textAlign, exp.textAlign);
+  });
 
 });
 
-describe('Functionality of Clear and Submit buttons', function () {
-    
-    it('Clear form button clears the bug form', function() {
-        help.login();
-        $(sel.newBug).click();
-        $(sel.summaryField).waitForDisplayed(waitForDisplayedTimeout);
-        bform.fillFieldsAndDropDownsInBugForm(sel.arrayFields(), sel.arrayDropDown());
-        let arrayFields = sel.arrayFields().map(str => $(str));
-        let arrayDropDown = sel.arrayDropDown().map(str => $(str));
-        $(sel.clear).click();
-        let fieldsValues = arrayFields.every(field => field.getValue() === "");
-        let dropDownValues = arrayDropDown.every(field => field.getValue() === null);
-        assert.equal(fieldsValues, true);
-        assert.equal(dropDownValues, true);
-    });
+describe('Submit', function () {
 
-    it('Bug Report page is opened after submitting a bug', function() {
-        bform.fillFieldsAndDropDownsInBugForm(sel.arrayFields(), sel.arrayDropDown());
-        $(sel.submit).click();
-        $(sel.openButton).waitForDisplayed(waitForDisplayedTimeout);
-        let openButton = $(sel.openButton).isDisplayed();
-        assert.isTrue(openButton);
-    });
+  it('Background Color', function () {
+    let subBackColor = $(sel.submitBut).getCSSProperty('background-color').parsed.hex;
+    assert.equal(subBackColor, exp.subBackColor);
+  });
 
-    it('Submit button adds new bug with specified values to the system', function() {
-        $(sel.newBug).click();
-        $(sel.summaryField).waitForDisplayed(waitForDisplayedTimeout);
-        bform.fillFieldsAndDropDownsInBugForm(sel.arrayFields(), sel.arrayDropDown());
-        let summary= $(sel.summaryField).getValue();
-        let priority = $(sel.priority).getText();
-        let assignee = $(sel.assignee).getText();
-        $(sel.submit).click();
-        $(sel.openButton).waitForDisplayed(waitForDisplayedTimeout);
-        $(sel.allIssues).click();
-        $(sel.bugSummary).waitForDisplayed(waitForDisplayedTimeout);
-        assert.equal($(sel.bugSummary).getText(), summary);
-        assert.equal($(sel.bugPriority).getText(), priority);
-        assert.equal($(sel.bugStatus).getText(), exp.status);
-        assert.equal($(sel.bugAssignee).getText(), assignee);
-    });
+  it(' Hover Background Color', function () {
+    $(sel.submitBut).moveTo();
+    browser.pause(200);
+    let hovBacColor = $(sel.submitBut).getCSSProperty('background-color').parsed.hex;
+    assert.equal(hovBacColor, exp.subHovBackColor);
+  });
+
 });
 
-describe('Functionality of version text field', function () {
+describe('Clear Form Button', function () {
 
-    const maxStringLength = 100;
-    const supportedCharacters =  "qA1!@#$%^&*() _+<>?:";
+  it('Background Color', function () {
+    let clearBackColor = $(sel.clearBut).getCSSProperty('background-color').parsed.hex;
+    assert.equal(clearBackColor, exp.clearBackColor);
+  });
 
-    it('Version field accepts any characters', function () {
-        help.login();
-        $(sel.newBug).click();
-        let version = $(sel.versionField);
-        version.setValue(supportedCharacters);
-        assert.equal(version.getValue(), supportedCharacters);
-    });
-    
-    it('Version field is always a single line', function () {
-        let version = $(sel.versionField);
-        let randomWithBreak = bform.generateRandomStringWithBreak(maxStringLength, supportedCharacters);
-        version.setValue(randomWithBreak);
-        assert.equal(version.getValue().includes('/\r\n/'), false);
-    });
-    
-    it('Version field accepts not more than 100 characters', function () {
-        let version = $(sel.versionField);
-        let randomString = bform.generateRandomString(maxStringLength + 1, supportedCharacters);
-        version.setValue(randomString);
-        assert.equal(version.getValue(), randomString.substr(0,maxStringLength));
-    });
-    
+  it(' Hover Background Color', function () {
+    $(sel.clearBut).moveTo();
+    browser.pause(200);
+    let hovBacColor = $(sel.clearBut).getCSSProperty('background-color').parsed.hex;
+    assert.equal(hovBacColor, exp.clearHovBackColor);
+  });
+
 });
