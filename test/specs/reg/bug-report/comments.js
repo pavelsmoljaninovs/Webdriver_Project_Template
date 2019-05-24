2125699dc8ab3ce8c0cda4106dd218926972592a
Natalia Osadcha <osadcha3@gmail.com>
2019-05-23 20:56:34 -0400

    updated

--- test/specs/reg/bug-report/comments.js
+++ test/specs/reg/bug-report/comments.js
@@ -3,53 +3,55 @@ import sel from '../../../selectors/bug-report';
 import exp from '../../../expected/bug-report-comments';
 import help from '../../../helpers/helpers';
 
- describe('Comments Title', function () {
+ // describe('Comments Title', function () {
+ //
+ //  it('Comments Title text', function () {
+ //    help.login();
+ //    $(sel.bugReport).click();
+ //    let commentsTitle = $(sel.commentsTitle).getText();
+ //    assert.equal(commentsTitle, exp.commentsTitle);
+ //  });
+ //
+ //  it('Comments Title text-align"', function () {
+ //    let actualTextAlign = $(sel.commentsTitle).getCSSProperty('text-align').value;
+ //    assert.equal(actualTextAlign, exp.commentsTitleTextAlign);
+ //  });
+ //
+ //  it('Comments Title font-weight"', function () {
+ //    let actualFontWeight = $(sel.commentsTitle).getCSSProperty('font-weight').value;
+ //    assert.equal(actualFontWeight, exp.commentsTitleFontWeight);
+ //  });
+ //
+ //  it('Comments Title font-family"', function () {
+ //    let actualFontFamily = $(sel.commentsTitle).getCSSProperty('font-family').value;
+ //    assert.equal(actualFontFamily, exp.commentsTitleFontFamily);
+ //  });
+ //
+ //  it('Comments Title font-size"', function () {
+ //    let actualFontSize = $(sel.commentsTitle).getCSSProperty('font-size').value;
+ //    assert.equal(actualFontSize, exp.commentsTitleFontSize);
+ //  });
+ //
+ //  it('Comments Title font-color"', function () {
+ //    let actualFontColor = $(sel.commentsTitle).getCSSProperty('color').parsed.hex;
+ //    assert.equal(actualFontColor, exp.commentsTitleFontColor);
+ //
+ //  });
 
-  it('Comments Title text', function () {
-    help.login();
-    $(sel.bugReport).click();
-    let commentsTitle = $(sel.commentsTitle).getText();
-    assert.equal(commentsTitle, exp.commentsTitle);
-  });
-
-  it('Comments Title text-align"', function () {
-    let actualTextAlign = $(sel.commentsTitle).getCSSProperty('text-align').value;
-    assert.equal(actualTextAlign, exp.commentsTitleTextAlign);
-  });
-
-  it('Comments Title font-weight"', function () {
-    let actualFontWeight = $(sel.commentsTitle).getCSSProperty('font-weight').value;
-    assert.equal(actualFontWeight, exp.commentsTitleFontWeight);
-  });
-
-  it('Comments Title font-family"', function () {
-    let actualFontFamily = $(sel.commentsTitle).getCSSProperty('font-family').value;
-    assert.equal(actualFontFamily, exp.commentsTitleFontFamily);
-  });
-
-  it('Comments Title font-size"', function () {
-    let actualFontSize = $(sel.commentsTitle).getCSSProperty('font-size').value;
-    assert.equal(actualFontSize, exp.commentsTitleFontSize);
-  });
-
-  it('Comments Title font-color"', function () {
-    let actualFontColor = $(sel.commentsTitle).getCSSProperty('color').parsed.hex;
-    assert.equal(actualFontColor, exp.commentsTitleFontColor);
-
-  });
-
-});
+//});
 
 describe('Comments List of comments (empty)', function () {
 
   it('Static text “No comments”', function () {
-    help.login();
-    $(sel.assign).click();
-    $(sel.bugReport).waitForDisplayed(true);
-    $(sel.summary2).click();
-    let actual = $$(sel.msgNoComments)[1].isDisplayed();
-    assert.equal(actual, true);
+      help.openBugReport();
   });
+  //   help.login();
+  //  // $(sel.assign).click();
+  //   $(sel.bugReport).waitForDisplayed(true);
+  //   $(sel.summary2).click();
+  //   let actual = $$(sel.msgNoComments)[1].isDisplayed();
+  //   assert.equal(actual, true);
+  // });
 
    it('Text-align', function () {
     let text = $$(sel.msgNoComments)[1].getCSSProperty('text-align').value;
@@ -75,5 +77,5 @@ describe('Comments List of comments (empty)', function () {
         let text = $$(sel.msgNoComments)[1].getCSSProperty('color').parsed.hex;
         assert.equal(text, exp.noCommentsTextFontColor);
     });
-});
+ });
 
