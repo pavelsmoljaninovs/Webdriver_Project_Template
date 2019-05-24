f6071864ca2b268582ee8d201688c566ee040c5f
Arthur Senko <arthursenko@gmail.com>
2019-05-24 00:29:59 -0700

    headless browser for smoke

--- smoke.conf.js
+++ smoke.conf.js
@@ -14,9 +14,12 @@ exports.config = {
 
   capabilities: [
     {
-      maxInstances: 1,
-      //
       browserName: 'chrome',
+      'goog:chromeOptions': {
+        // to run chrome headless the following flags are required
+        // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+        args: ['--headless', '--disable-gpu'],
+      }
     }
   ],
 
