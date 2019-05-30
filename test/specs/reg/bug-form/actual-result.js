8b5e7e2dca64f942dce1e5d86953744b97c74265
dmytroartamonov <artamonoph@gmail.com>
2019-05-29 21:20:08 -0500

    changed

--- test/specs/reg/bug-form/actual-result.js
+++ test/specs/reg/bug-form/actual-result.js
@@ -1,49 +1,43 @@
-import { assert } from 'chai';
+import {assert} from 'chai';
 import sel from '../../../selectors/bug-form';
 import exp from '../../../expected/bug-form';
-import help from "../../../helpers/helpers";
+import help from '../../../helpers/helpers';
 
 describe('actual result', function () {
 
     it('Font Family', function () {
         help.login();
         $(sel.newBug).click();
-        let element = $(sel.actualField);
+        let element = $(sel.actualResult);
         let fontFamily = element.getCSSProperty('font-family').value;
         assert.equal(fontFamily, exp.fontFamily);
     });
 
     it('Font Size', function () {
-
         $(sel.newBug).click();
-        let element = $(sel.actualField);
+        let element = $(sel.actualResult);
         let fontSize = element.getCSSProperty('font-size').value;
         assert.equal(fontSize, exp.fontSize);
     });
 
     it('Font Weight', function () {
-
         $(sel.newBug).click();
-        let element = $(sel.actualField);
+        let element = $(sel.actualResult);
         let fontWeight = element.getCSSProperty('font-Weight').value;
         assert.equal(fontWeight, exp.fontWeight);
     });
 
     it('Font Color', function () {
-
         $(sel.newBug).click();
-        let element = $(sel.actualField);
+        let element = $(sel.actualResult);
         let fieldFontColor = element.getCSSProperty('color').parsed.hex;
         assert.equal(fieldFontColor, exp.fieldFontColor);
     });
 
     it('text align', function () {
-
         $(sel.newBug).click();
-        let element = $(sel.actualField);
+        let element = $(sel.actualResult);
         let fieldTextAlign = element.getCSSProperty('text-align').value;
         assert.equal(fieldTextAlign, exp.fieldTextAlign);
     });
-
-
 });
