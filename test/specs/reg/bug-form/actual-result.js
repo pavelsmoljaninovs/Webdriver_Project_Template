aa75842316b76819719c59500eaf4529bf91363d
dmytroartamonov <artamonoph@gmail.com>
2019-05-30 11:51:31 -0500

    changed code actual-result

--- test/specs/reg/bug-form/actual-result.js
+++ test/specs/reg/bug-form/actual-result.js
@@ -14,28 +14,24 @@ describe('actual result', function () {
     });
 
     it('Font Size', function () {
-        $(sel.newBug).click();
         let element = $(sel.actualResult);
         let fontSize = element.getCSSProperty('font-size').value;
         assert.equal(fontSize, exp.fontSize);
     });
 
     it('Font Weight', function () {
-        $(sel.newBug).click();
         let element = $(sel.actualResult);
         let fontWeight = element.getCSSProperty('font-Weight').value;
         assert.equal(fontWeight, exp.fontWeight);
     });
 
     it('Font Color', function () {
-        $(sel.newBug).click();
         let element = $(sel.actualResult);
         let fieldFontColor = element.getCSSProperty('color').parsed.hex;
         assert.equal(fieldFontColor, exp.fieldFontColor);
     });
 
     it('text align', function () {
-        $(sel.newBug).click();
         let element = $(sel.actualResult);
         let fieldTextAlign = element.getCSSProperty('text-align').value;
         assert.equal(fieldTextAlign, exp.fieldTextAlign);
