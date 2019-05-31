eae260887f0aee73d02f6353020612cff1888aeb
Arthur Senko <arthursenko@gmail.com>
2019-05-31 12:16:10 -0700

    report email

--- send-email.js
+++ send-email.js
@@ -3,21 +3,6 @@ let emails = require('./config/emails');
 let gmail = require('./config/gmail');
 let report = require('./allure-report/widgets/summary.json');
 
-function generateHTML(report) {
-  let passedWidth = report.statistic.passed / report.statistic.total * 100;
-
-  return `<div style="position:relative;">` +
-    `<hr style="position:absolute; top:0; left:0;" color="red" align=left  size=20 width="100%" noshade>` +
-    `<hr style="position:absolute; top:0; left:0;" color="green" align=left  size=20 width="${passedWidth}%" noshade></div>` +
-    `<div style="position: relative; padding-top: 30px;">` +
-    `<table width="100%" border="1"><thead>` +
-    `<tr align="center"><th>Total</th><th>Failed</th><th>Passed</th><th>Broken</th><th>Skipped</th><th>Unknown</th></tr>` +
-    `</thead><tbody>` +
-    `<tr align="center"><td>${report.statistic.total}</td><td>${report.statistic.failed}</td><td>${report.statistic.passed}</td>` +
-    `<td>${report.statistic.broken}</td><td>${report.statistic.skipped}</td><td>${report.statistic.unknown}</td></tr>` +
-    `</tbody></table></div>`
-}
-
 let transporter = nodemailer.createTransport({
   pool: true,
   host: gmail.host,
@@ -32,7 +17,7 @@ let transporter = nodemailer.createTransport({
 let mailOptions = {
   from: 'react.bug.tracker@gmail.com',
   to: emails.join(', '),
-  subject: 'Automation Testing has finished',
+  subject: `Automation Testing has finished`,
   html: generateHTML(report)
 };
 
@@ -45,3 +30,29 @@ transporter.sendMail(mailOptions, function (error, info) {
     process.exit();
   }
 });
+
+function generateHTML(report) {
+  let passedWidth = report.statistic.passed / report.statistic.total * 100;
+
+  return `<div>Duration: ${duration(report.time.duration)}</div>` +
+
+  `<div>Results:</div>` +
+    `<div style="position:relative;">` +
+    `<table width="100%" style="height: 20px;"><tr><td style="width:${passedWidth}%; background-color: forestgreen;"></td>` +
+    `${passedWidth < 100 ? `<td style="width:${100 - passedWidth}%; background-color: red;"></td>` : ``}`+
+    `</tr></table>` +
+
+    `<div style="position: relative;">` +
+    `<table width="100%" border="1"><thead>` +
+    `<tr align="center"><th>Total</th><th>Failed</th><th>Passed</th><th>Broken</th><th>Skipped</th><th>Unknown</th></tr>` +
+    `</thead><tbody>` +
+    `<tr align="center"><td>${report.statistic.total}</td><td>${report.statistic.failed}</td><td>${report.statistic.passed}</td>` +
+    `<td>${report.statistic.broken}</td><td>${report.statistic.skipped}</td><td>${report.statistic.unknown}</td></tr>` +
+    `</tbody></table></div>`
+}
+
+function duration(ms){
+  let min = parseInt(ms / 60000);
+  let sec = parseInt((ms % 60000) / 1000);
+  return `${min} min ${sec} sec`
+}
