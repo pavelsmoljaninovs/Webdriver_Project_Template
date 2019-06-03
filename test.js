780b5698d836d036dfa4530c0a199f75bd2ce5a4
artsenius <34055997+artsenius@users.noreply.github.com>
2019-06-03 15:28:01 -0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://small-express-server.herokuapp.com/sgfsdgzg')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error.response.data);
-  });
